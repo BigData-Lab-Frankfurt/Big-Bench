aaa78db3317857b40bb0c2c475ff11fec4ed666d
mxbeer <max-beer@web.de>
2015-03-08 20:50:07 +0100

    make udfs running with spark

--- engines/spark/queries/q10/run.sh
+++ engines/spark/queries/q10/run.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+JAR_FILE1="$BIG_BENCH_QUERIES_DIR/Resources/opennlp-maxent-3.0.3.jar"
+JAR_FILE2="$BIG_BENCH_QUERIES_DIR/Resources/opennlp-tools-1.5.3.jar"
+JAR_FILE3="$BIG_BENCH_QUERIES_DIR/Resources/bigbenchqueriesmr.jar"
+
+BINARY_PARAMS="$BINARY_PARAMS --jars $JAR_FILE1,$JAR_FILE2,$JAR_FILE3"
+
 query_run_main_method () {
 	QUERY_SCRIPT="$QUERY_DIR/$QUERY_NAME.sql"
 	if [ ! -r "$QUERY_SCRIPT" ]
